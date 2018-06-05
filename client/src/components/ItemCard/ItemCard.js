317ef86c19e7908de3dcffadc4b1a0404bd2152f
unknown <arpeterson78@gmail.com>
2018-06-05 15:13:11 -0500

    added item card component

---
+++ client/src/components/ItemCard/ItemCard.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+class ItemCard extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+
+        return (
+            <div key={this.props.key} className="col s12 m3">
+                <div className="card" style={{ textAlign: "center" }}>
+                    <div className="card-image" style={{ textAlign: "center" }}>
+                        <img src={process.env.PUBLIC_URL + this.props.attribute.image_url} style={{ height: "150px", width: "80px", paddingTop: "10px", display: "inline" }} />
+                    </div>
+                    <div className="card-content" style={{ paddingTop: "0px" }}>
+                        <h4>{this.props.attribute.product_name}</h4>
+                        <p>Price: ${this.props.attribute.price}</p>
+                        <p>Quantity: {this.props.attribute.quantity}</p>
+                    </div>
+                    <div className="card-action">
+                        <button className="btn">Add to Cart</button>
+                    </div>
+                </div>
+            </div>
+        );
+
+    }
+
+}
+
+export default ItemCard;
